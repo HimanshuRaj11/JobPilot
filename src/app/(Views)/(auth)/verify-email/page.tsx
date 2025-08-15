e3e8b70ad0d72ea64270a3a401f4909cf50d484d
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-08-15 20:24:25 +0530

    commit to add email verification method

--- src/app/(Views)/(auth)/verify-email/page.tsx
+++ src/app/(Views)/(auth)/verify-email/page.tsx
@@ -1,42 +1,14 @@
-'use client'
-import axios from 'axios';
-import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from "react-redux";
-import { FetchUser } from "@/app/Redux/Slice/User.slice";
-import { toast } from 'react-toastify';
+
+"use client";
+
+import VerifyEmailContent from "@/components/VerifyEmailContent";
+import { Suspense } from "react";
+
 
 export default function page() {
-    const params = useSearchParams();
-    const token = params.get("token");
-    const [message, setMessage] = useState("Verifying...");
-    const dispatch = useDispatch()
-    const router = useRouter()
-
-    const verifyEmail = async () => {
-        try {
-            if (!token) {
-                setMessage("Invalid verification link.");
-                return;
-            }
-            const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/verify-email?token=${encodeURIComponent(token as string)}`, { withCredentials: true })
-            console.log(data);
-
-            if (data.success) {
-                toast.success(data.message)
-                dispatch(FetchUser() as any);
-                router.push('/')
-            }
-        } catch (error) {
-            return error
-        }
-    }
-
-    useEffect(() => {
-
-        verifyEmail()
-    }, []);
     return (
-        <div>{message}</div>
-    )
+        <Suspense fallback={<div className="p-4 text-gray-500">Loading...</div>}>
+            <VerifyEmailContent />
+        </Suspense>
+    );
 }
