6edf643213f031415fa8c9da277e9c02ca541f11
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2025-08-15 16:09:42 +0530

    almost complete

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    project_Id: process.env.FIREBASE_PROJECT_ID,
+    project_id: process.env.FIREBASE_PROJECT_ID,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -5,10 +5,11 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAeOWLvH7N5t5W3Ju_fKtJC5sCFHqzhLFU",
     authDomain: "jobpilot-de59a.firebaseapp.com",
-    projectId: "jobpilot-de59a",
+    project_id: "jobpilot-de59a",
     storageBucket: "jobpilot-de59a.firebasestorage.app",
     messagingSenderId: "1012838762687",
     appId: "1:1012838762687:web:8226f00cce4cbbaaa3de0a",
